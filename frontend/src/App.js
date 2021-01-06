ca0d7e252d37371f5b1b96fe7612c1fb5aba293b
Dilya Joseph <dilyajoseph@gmail.com>
2021-01-06 15:05:07 -0600

    bootstrap added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
-import { useState } from 'react';
-
-function App() {
+import { useState, useRef, useEffect } from 'react';
+export default (props) => {
   const [recipes, setRecipes] = useState([])
+  const ingredientsInput = useRef(null);
+
+
+  const searchRecipe = async (event) => {
+    try {
+      const response = await fetch('http://localhost:3000/recipes', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        }
+      })
+      const data = await response.json();
+      props.searchRecipes([...props.recipes, data])
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   // Read 
   const fetchRecipes = async () => {
@@ -13,6 +29,8 @@ function App() {
       console.log(error)
     }
   }
+  
+
 
   // Delete
   const deleteRecipe = async (id) => {
@@ -34,10 +52,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>List your ingrediants</h1>
-      <input type="submit"></input>
+        
+        <h1>What's For Dinner?</h1> <br/>
+        <h2>List some ingredients here..</h2>
+      <form onSubmit={searchRecipe}>
+            <input type="text" ref={ingredientsInput} />
+            <input type="submit" value="Search for Recipe"/>
+        </form>
+        <ul>
+        {
+          recipes.map(recipe => {
+            return (
+            <li key={recipe._id}>{`${recipe.name}`}<br/>
+            <button onClick={
+              (event) => {
+                searchRecipe(recipe._id)
+              }
+            }> {recipe.name} </button>
+            </li>
+            )
+          })
+        }
+        </ul>
       </header>
     </div>
   );
 }
-export default App;
+
+
